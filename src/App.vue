ecec665e921c77b6679e7875a5b1cd6f1ff8006d
mashud.karim <mashud.karim85@gmail.com>
2023-08-12 19:34:39 +0600

    complete hooks assignment

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Carousel from './components/Carousel.vue'
+import PieChart from './components/PieChart.vue'
+import { ref } from 'vue'
+let currentIndex = ref(0)
+
+function carouselData () {
+  currentIndex.value = 0;
+}
+
+function pieChartData () {
+  currentIndex.value = 1;
+}
+</script>
+
+<template>
+    <div class="flex">
+        <button type="button" @click="carouselData"  class="me-3" :class="0==currentIndex ? 'btn btn-primary': ''">Image Carousel</button>
+        <button type="button" @click="pieChartData" :class="1==currentIndex ? 'btn btn-primary': ''">Pie Chart</button>
+      </div>
+    <Carousel v-if="0== currentIndex"/>
+    <PieChart  v-if="1== currentIndex"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/PieChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="justify-center mt-5">
+    <h3>Vue Pie Chart Example</h3>
+    <GChart
+      type="PieChart"
+      :options="options"
+      :data="data"
+    />    
+  </div>
+</template>
+ 
+<script setup>
+import { onMounted, onBeforeUnmount } from "vue";
+import { GChart } from "vue-google-charts";
+
+const data = [
+  ['Daily Routine', 'Hours per Day'],
+  ['Work',     14],
+  ['Eat',      1],
+  ['Reading',  2]
+  
+];
+
+const options = {
+  width: 1000,
+  height: 600
+};
+
+onMounted(() => {
+  console.log('Pie chart onMounted hooks call')
+  data.push(['Exercise', 2], ['Sleep',    5])
+})
+
+onBeforeUnmount(() => {
+  console.log('Pie chart onBeforeUnmount hooks call')
+  data.splice(4, 2);
+});
+
+</script>
